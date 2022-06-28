b043f6fb6dc393e52cd6e11695e5d3bb875f83ab
terra-d <dummy@email.com>
2022-06-29 02:15:47 +0900

    remove unused value

--- src/ch01/listing1_6/listing1_6.js
+++ src/ch01/listing1_6/listing1_6.js
@@ -1,5 +1,4 @@
 window.addEventListener('DOMContentLoaded', (_event) => {
-  let valid = false;
   const elem = document.querySelector('#student-ssn');
   elem.onkeyup = function (_e) {
     let val = elem.value;
@@ -7,7 +6,6 @@ window.addEventListener('DOMContentLoaded', (_event) => {
       val = val.replace(/^\s*|\s*$|\-/g, '');
       if (val.length === 9) {
         console.log(`Valid SSN: ${val}!`);
-        valid = true;
       }
     } else {
       console.log(`Invalid SSN: ${val}!`);
