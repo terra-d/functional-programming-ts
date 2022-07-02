6431804dce52700af544065c75b70fcd60605237
terra-d <dummy@email.com>
2022-07-02 15:52:10 +0900

    convert ts

--- src/ch01/listing1_7/listing1_7.js
+++
@@ -1,7 +0,0 @@
-Rx.Observable.fromEvent(document.querySelector('#student-ssn'), 'keyup')
-  .pluck('srcElement', 'value')
-  .map((ssn) => ssn.replace(/^\s*|\s*$|\-/g, ''))
-  .filter((ssn) => ssn !== null && ssn.length === 9)
-  .subscribe((validSsn) => {
-    console.log(`Valid SSN ${validSsn}`);
-  });
---
+++ src/ch01/listing1_7/listing1_7.ts
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', _event => {
+  Rx.Observable.fromEvent(document.querySelector('#student-ssn'), 'keyup')
+    .pluck('srcElement', 'value')
+    .map((ssn) => ssn.replace(/^\s*|\s*$|\-/g, ''))
+    .filter((ssn) => ssn !== null && ssn.length === 9)
+    .subscribe((validSsn) => {
+      console.log(`Valid SSN ${validSsn}`);
+    });
+})
